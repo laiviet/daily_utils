0c78a043e35e420597317f9032babb36d53e9d23
viet-lai <viet.lai@kensho.com>
2024-05-09 09:08:03 -0400

    Add read/write text files

--- build_and_push_to_pypi.sh
+++ build_and_push_to_pypi.sh
@@ -2,10 +2,12 @@
 
 git add -u
 git commit -m "Add read/write text files"
-
+git push
 # Build
 python -m build
 
 
-# Upload
-twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+# Upload to TestPyPi
+#twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+
+twine upload dist/*
